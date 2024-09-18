47edd662b0448d51b34b3ef26bb164ff14a45820
showshant <sushantbasnet06@gmail.com>
2024-09-18 16:11:06 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail= "sushantbasnet01@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kathmandu"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "kfhld@yahhoo.com"
+accountPassword = "2125"
+accountCity = "Bhaktapur"
+
+console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js, not browser
+
+
+let name = "Sushant"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
