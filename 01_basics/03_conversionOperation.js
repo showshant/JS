2ca5c59a6e953f161f857c528436282fd317835a
showshant <sushantbasnet06@gmail.com>
2024-09-19 02:32:29 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,9 +22,57 @@ let score = "sushant"
 // ""=> false
 // "sushant" => true
 
-let someNumber = 33
+// let someNumber = 33
  
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***************Operations***********
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 raise 3
+// console.log(2/2);
+// console.log(6%3);
+
+// let str1 = "Hello"
+// let str2 = " Sushant"
+
+// let str3= str1 + str2;
+// console.log(str3);
+
+// console.log("2" + 5)
+// console.log(2 + "5")
+// console.log("2" + 2 + 5)
+// console.log(2 + 2 + "5")
+
+// console.log("2" + "5")
+
+// console.log(true+2);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+// console.log(num3);
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
 
