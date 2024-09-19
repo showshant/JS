4eb3abd16e7411e09dd561148a73a788c70488b3
showshant <sushantbasnet06@gmail.com>
2024-09-19 02:33:55 +0000

    dataTypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//comparisons like < > <= >= convert null to a number treating it as 0.
+
+// console.log("2" === 2);
+// console.log("2" == 2);
