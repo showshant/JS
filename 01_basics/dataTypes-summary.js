72d729f978f11cda3c9f0630c00fea82999519ed
showshant <sushantbasnet06@gmail.com>
2024-09-19 02:40:00 +0000

    summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,14 @@
+/*
+Primitive datatypes:
+String - string
+Number - number
+Boolean - Boolean
+Null - object(****)
+Undefined - undefined
+BigInt - bigint
+
+Non-primitive datatypes:
+Array - object
+Function - function(object)
+Object - object 
+*/
