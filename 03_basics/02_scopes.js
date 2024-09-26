206d6dfde856549618eac60d5857c90f6374ba86
showshant <sushantbasnet06@gmail.com>
2024-09-26 14:33:44 +0000

    update

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,53 @@
+let a = 10;
+const b = 20;
+var c = 30;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one (){
+    const username = "sushant"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+    
+}
+
+// one()
+
+if(true){
+    const username = "sushant"
+    if(username === "sushant"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website); 
+    
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++ interesting ++++++++++++++++++
+
+
+function addone(num){
+    return num + 1; 
+}
+
+addone(5)
+
+
+
+const addTwo= function(num){
+    return num + 2
+}
+
+addTwo(5)
