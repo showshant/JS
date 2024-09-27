385678fa83d3447b2caba61fd73e8ec75bee9553
showshant <sushantbasnet06@gmail.com>
2024-09-27 06:48:12 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`Value of index is: ${i}`);
+//        i = i + 2;
+// }
+
+// let myArray = ['flash', 'batman', 'superman']
+// 
+// let arr = 0;
+// while (arr<myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+    // arr = arr + 1;
+// }
+
+
+// for loop
+// let myArray = ['sushant', 'messi', 'neymar']
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i])
+    
+// }
+
+//do while loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score<=10);
