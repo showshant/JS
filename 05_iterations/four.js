d82033fb7dc3a1146f61d5e3438346603f670134
showshant <sushantbasnet06@gmail.com>
2024-09-27 16:59:53 +0000

    done

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'Swift by apple'
+}
+
+for (const key in myObject) {
+//   console.log(`${key} shortcut is for ${myObject[key]}`);
+  
+}
+
+const programming = ['js', 'python', 'java', 'cpp']
+
+for (const key in programming) {
+   console.log(programming[key]);
+   
+        
+    }
+
+//     const map = new Map()
+//     map.set('IN', 'India')
+//     map.set('FR', 'France')
+//     map.set('US', 'United States of America')
+    
+// for (const key in map) {
+//    console.log(key);
+   
+// }
+
