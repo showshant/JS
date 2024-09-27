86eb4bda30972a619d1070d055c08b3324171163
showshant <sushantbasnet06@gmail.com>
2024-09-27 03:40:46 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = "april"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "4":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
