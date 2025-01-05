6a6658a1e896c7eb1105536f7441a819175f8642
showshant <sushantbasnet06@gmail.com>
2025-01-05 15:10:31 +0000

    call, apply and bind

---
+++ call_apply_bind.js
@@ -0,0 +1,40 @@
+// call apply bind
+
+// const obj = {
+//   name: "sushant",
+//   surname: "basnet",
+//   age : 21,
+// };
+// function abcd() {
+//   console.log(this);
+// }
+
+// abcd.call("12");
+
+//apply
+// const obj = {
+//     name: "sushant",
+//     surname: "basnet",
+//     age : 21,
+//   };
+
+//   function abcd(a, b, c) {
+//     console.log(this, a, b, c);
+//   }
+
+//   abcd.apply(obj, [1,2,3]);
+
+//bind
+
+// const obj = {
+//   name: "sushant",
+//   surname: "basnet",
+//   age: 21,
+// };
+
+// function abcd() {
+//   console.log(this);
+// }
+
+// const later = abcd.bind(obj);
+// later();
