b36205c2ef89a500093b478795b980f1cafc34df
showshant <sushantbasnet06@gmail.com>
2025-01-05 17:33:51 +0000

    hof

---
+++ 09_fro_end_dom/hof.js
@@ -0,0 +1,17 @@
+// higher order functions
+
+// any function that accepts another function as parameter and/or returns a function
+
+// function abcd(param){
+
+// }
+// abcd(function(){})
+
+function abcd(param) { //here abcd is a higher order function
+    return function () {};
+  }
+  
+  
+  var arr = [1, 2, 3, 4, 5, 6]
+  
+  arr.forEach(function(){}) //forEach is a hof
