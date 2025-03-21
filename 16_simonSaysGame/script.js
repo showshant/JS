c2c5b8f991e647cca47726cc5080a131f241b9aa
showshant <sushantbasnet06@gmail.com>
2025-03-21 16:30:33 +0000

    simon

---
+++ 16_simonSaysGame/script.js
@@ -0,0 +1,25 @@
+let gameSequence = [];
+let userSequence = [];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector('h2')
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("game is started");
+    started = true;
+    levelUp();
+  }
+});
+
+function btnFlash(){
+    
+}
+
+function levelUp() {
+  level++;
+  h2.innerText = `level ${level}`
+}
+
+btnFlash();
