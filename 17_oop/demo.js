1b3aee55412a84b874cc458d70e78480c039b35e
showshant <sushantbasnet06@gmail.com>
2025-03-24 13:58:34 +0000

    oop 1

---
+++ 17_oop/demo.js
@@ -0,0 +1,21 @@
+class Product {
+    name;
+    proce;
+    category;
+    description;
+    rating;
+  
+    addToCart() {
+      console.log("Product added to cart");
+    }
+    removeFromCart() {
+      console.log("Product removed from cart");
+    }
+    displayProduct() {
+      console.log("Product displayed");
+    }
+    buyProduct() {
+      console.log("Product bought");
+    }
+  }
+  
